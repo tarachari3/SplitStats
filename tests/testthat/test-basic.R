892c9e69e8d044e0143989fa4b05fbdbf3e16b57
tarachari3 <tarachari3@gmail.com>
2021-08-11 11:45:14 -0700

    Change file.path

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -5,7 +5,7 @@ test_that("internal test package installs correctly", {
   xpath <- basilisk.utils::getExternalDir()
 
   output <- SplitStats::test()
-  expect_true(file.exists(file.path(xpath, "SplitStats/0.1.0/env4")))
+  expect_true(file.exists(file.path(xpath,"SplitStats","0.1.0","env4")))
 
   expect_type(output$splitspy, "character")
   expect_true(length(output$splitspy) > 0L)
